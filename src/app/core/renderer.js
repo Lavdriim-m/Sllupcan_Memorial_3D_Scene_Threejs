8fbc3d7f57e4db109d76a11a00d18a90b89b10d9
Lavdriim-m <lavdrim.mustafi03@gmail.com>
2026-02-03 03:11:32 +0100

    cleaned some code.

--- src/app/core/renderer.js
+++ src/app/core/renderer.js
@@ -1,20 +1,20 @@
 import * as THREE from "three";
 
 export function createRenderer(canvas, sizes) {
-  const renderer = new THREE.WebGLRenderer({
-    canvas,
-    antialias: true,
-  });
+    const renderer = new THREE.WebGLRenderer({
+        canvas,
+        antialias: true,
+    });
 
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-  // Shadows (requirement)
-  renderer.shadowMap.enabled = true;
-  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    // Shadows (requirement)
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-  // Slightly nicer output
-  renderer.outputColorSpace = THREE.SRGBColorSpace;
+    // Slightly nicer output
+    renderer.outputColorSpace = THREE.SRGBColorSpace;
 
-  return renderer;
+    return renderer;
 }
